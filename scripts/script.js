e698523a23b135c8dfe52deb751af21550559c80
Ahmed Asafrah <211033@ppu.edu.ps>
2024-10-29 02:13:05 +0200

    Fetch and display book data from API

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,69 @@
-const bookContainer = document.querySelector(".container");
-const books = [];
-const defaultBook = {
-  title: "",
-  author: "",
-  type: "",
-  img: "",
+const bookList = () => {
+  return fetch("https://mocki.io/v1/443f4adf-3c7b-4187-bad1-0a83dc05c571").then(
+    (response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("An error occurred: " + response.status);
+      }
+    }
+  );
 };
-const bookFormModal = document.getElementById("book-form-modal");
-const addBookButton = document.getElementById("add-book-button");
+
+const BOOKs = [];
+
+const loadData = () => {
+  bookList()
+    .then((data) => {
+      data.forEach((book) => {
+        BOOKs.push(book);
+      });
+      renderBooks();
+    })
+    .catch((error) => {
+      console.error("Failed to load data:", error);
+    });
+};
+
+const renderBooks = () => {
+  const container = document.querySelector(".container");
+  container.innerHTML = ""; // Clear the container
+  BOOKs.forEach((book) => {
+    container.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="book">
+        <img src="${book.img}" alt="book" />
+        <h3>${book.title}</h3>
+        <p>${book.author}</p>
+        <p>${book.type}</p>
+        <div>
+          <button id="toggle">Read</button>
+          <button id="plus">+</button>
+        </div>
+      </div>
+      `
+    );
+  });
+};
+
+const addBook = () => {
+  const bookTitle = document.getElementById("book-title").value;
+  const bookAuthor = document.getElementById("book-author").value;
+  const bookGenre = document.getElementById("book-type").value;
+  const bookImg = document.getElementById("book-img").value;
+
+  const newBook = {
+    title: bookTitle,
+    author: bookAuthor,
+    type: bookGenre,
+    img: bookImg,
+  };
+
+  BOOKs.push(newBook);
+  renderBooks();
+  closeForm();
+};
 
 const openForm = () => {
   document.getElementById("book-title").value = "";
@@ -15,44 +71,15 @@ const openForm = () => {
   document.getElementById("book-type").value = "";
   document.getElementById("book-img").value = "";
 
+  const bookFormModal = document.getElementById("book-form-modal");
   bookFormModal.style.display = "block";
   document.body.style.overflow = "hidden";
 };
 
 const closeForm = () => {
+  const bookFormModal = document.getElementById("book-form-modal");
   bookFormModal.style.display = "none";
   document.body.style.overflow = "auto";
 };
 
-const renderBook = (book = defaultBook) => {
-  const bookHtml = `
-        <div class="book">
-            <img src="${book.img}" alt="book" />
-            <h3>${book.title}</h3>
-            <p>${book.author}</p>
-            <p>${book.type}</p>
-            <div>
-                <button id="toggle">Read</button>
-                <button id="plus">+</button>
-            </div>
-        </div>
-    `;
-  bookContainer.innerHTML += bookHtml;
-};
-
-const addBook = () => {
-  const title = document.getElementById("book-title").value;
-  const author = document.getElementById("book-author").value;
-  const type = document.getElementById("book-type").value;
-  const img = document.getElementById("book-img").value;
-
-  const newBook = { title, author, type, img };
-  books.push(newBook);
-  renderBook(newBook);
-  closeForm();
-};
-
-addBookButton.onclick = addBook;
-
-const addBookIcon = document.querySelector(".add-book");
-addBookIcon.onclick = openForm;
+loadData();
